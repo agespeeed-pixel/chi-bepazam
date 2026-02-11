0b8846596c33bd818e3cfd42c72fb750f0808f68
agespeeed-pixel <age.speeed@gmail.com>
2026-02-11 11:18:24 +0330

    Create main.dart as entry point of the Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// Main entry point for the Flutter application
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to My Flutter App'),
+        ),
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
