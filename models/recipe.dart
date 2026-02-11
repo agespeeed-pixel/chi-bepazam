cdb83c05c20c3f34bde6737b0e1532a9440932ae
agespeeed-pixel <age.speeed@gmail.com>
2026-02-11 11:20:57 +0330

    Create Recipe class for recipe data structure

---
+++ models/recipe.dart
@@ -0,0 +1,7 @@
+class Recipe {
+    String name;
+    List<String> ingredients;
+    String instructions;
+
+    Recipe({required this.name, required this.ingredients, required this.instructions});
+}
